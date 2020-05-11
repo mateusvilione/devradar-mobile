5ac6377a982f1f8171e45ce9eb206c92f6f8f016
Mateus Vilione <mateusvilione@gmail.com>
2020-05-11 15:20:35 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://192.168.0.129:3333",
+    baseURL: "https://devradar-back-end.herokuapp.com",
 });
 
 export default api;
